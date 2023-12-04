f86ab9f1e12df5dc19d69203dd7fbf3cad563bb0
RUPANCHAKRAVARTHY <rupanrrc.chak@gmail.com>
2023-12-04 16:50:17 +0530

    completed

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from 'firebase/app';
+
+//  Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: 'mern-estate.firebaseapp.com',
+  projectId: 'mern-estate',
+  storageBucket: 'mern-estate.appspot.com',
+  messagingSenderId: '1078482850952',
+  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
